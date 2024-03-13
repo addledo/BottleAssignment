0eaecedbd835e1ac488835c6f62c5ab05f973c85
addledo <jontyfpine@gmail.com>
2024-03-13 22:34:04 +0000

    Wrote more tests

---
+++ Tests/BottleBrandComparatorTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BottleBrandComparatorTest {
+    ArrayList<Bottle> testBottles = new ArrayList<>();
+    @BeforeEach
+    void setUp() {
+        testBottles.add(new Bottle("D", 500, Material.METAL, ""));
+        testBottles.add(new Bottle("C", 1, Material.PLASTIC, "Something"));
+        testBottles.add(new Bottle("A", 10000, Material.GLASS, "1234"));
+        testBottles.add(new Bottle("B", 0, Material.GLASS));
+    }
+
+    @AfterEach
+    void tearDown() {
+        testBottles = new ArrayList<>();
+    }
+
+    @Test
+    void compare() {
+        BottleBrandComparator brandComparator = new BottleBrandComparator();
+        testBottles.sort(brandComparator);
+        assertEquals("A", testBottles.get(0).getBrand());
+        assertEquals("B", testBottles.get(1).getBrand());
+        assertEquals("C", testBottles.get(2).getBrand());
+        assertEquals("D", testBottles.get(3).getBrand());
+    }
+}
