bc191d6fce327583d6bf23c9ea03d4e37fe8cb3f
addledo <jontyfpine@gmail.com>
2024-03-14 15:38:17 +0000

    Tidied up Material enum

--- src/Material.java
+++ src/Material.java
@@ -1,21 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public enum Material {
     PLASTIC("Plastic"),
     METAL("Metal"),
     GLASS("Glass");
 
-    final String displayName;
+    public final String displayName;   //Made public as field is final and immutable
 
     Material(String displayName) {
         this.displayName = displayName;
     }
 
-//    public String getDisplayName() {
-//        String name = this.name();
-////    ArrayList<String> letters = (ArrayList<String>) Arrays.asList(name.split("_"));
-//        String[] letters = name.split()
-//        return name;
-//    }
 }
